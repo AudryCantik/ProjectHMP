4a6ad50fab85d595e65a90d39eff0fdc91f652b8
AnastasyaP <anastasyap41@gmail.com>
2024-11-25 17:40:31 +0700

    fixed template

--- Project_HMP/src/app/app-routing.module.ts
+++ Project_HMP/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',  // Mengarahkan path kosong ke halaman 'home'
+    redirectTo: 'whatweplay',  // Mengarahkan path kosong ke halaman 'home'
     pathMatch: 'full'
   }, 
 
@@ -14,14 +14,24 @@ const routes: Routes = [
   },
 
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'whatweplay', 
+    loadChildren: () => import('./whatweplay/whatweplay.module').then(m => m.WhatweplayPageModule)
+  },
+
+  {
+    path: 'whoweare', 
+    loadChildren: () => import('./whoweare/whoweare.module').then(m => m.WhowearePageModule)
+  },
+
+  {
+    path: 'ourschedule', 
+    loadChildren: () => import('./ourschedule/ourschedule.module').then(m => m.OurschedulePageModule)
   },
 
   {
     path: 'scheduledetails/:name', // Adding the route for ScheduledetailsPage with a dynamic ID parameter
-    loadChildren: () => import('./home/scheduledetails/scheduledetails.module').then(m => m.ScheduledetailsPageModule)
-  }
+    loadChildren: () => import('./scheduledetails/scheduledetails.module').then(m => m.ScheduledetailsPageModule)
+  },
 
 ];
 @NgModule({
--- Project_HMP/src/app/home/home-routing.module.ts
+++ Project_HMP/src/app/home/home-routing.module.ts
@@ -8,18 +8,6 @@ const routes: Routes = [
     path: '',
     component: HomePage
   },
-  {
-    path: 'whoweare',
-    loadChildren: () => import('./whoweare/whoweare.module').then( m => m.WhowearePageModule)
-  },
-  {
-    path: 'whatweplay',
-    loadChildren: () => import('./whatweplay/whatweplay.module').then( m => m.WhatweplayPageModule)
-  },
-  {
-    path: 'ourschedule',
-    loadChildren: () => import('./ourschedule/ourschedule.module').then( m => m.OurschedulePageModule)
-  }
 
 ];
 
